c067891420505274f69ea9f542c3d53604165039
i <marcel.frankruijter@mediact.nl>
2017-10-24 16:57:10 +0200

    - Send the fee including tax to the CommonTaxCollector and changed the flag "TAXABLE_PRICE_INCLUDES_TAX" to true. - Calculated the basePaymentFee in Quote/BuckarooFee.php based on existing parameters instead of refetching it. - Added a incl_tax flag to the function getBaseFee to return the original tax included price of the fee (the one from the database).

--- Model/Total/Quote/BuckarooFee.php
+++ Model/Total/Quote/BuckarooFee.php
@@ -129,7 +129,7 @@ class BuckarooFee extends \Magento\Quote\Model\Quote\Address\Total\AbstractTotal
             return $this;
         }
 
-        $basePaymentFee = $this->getBaseFee($methodInstance, $quote);
+        $basePaymentFee = $total->getBaseBuckarooFeeInclTax() - $total->getBuckarooFeeBaseTaxAmount();
 
         if ($basePaymentFee < 0.01) {
             return $this;
@@ -202,7 +202,8 @@ class BuckarooFee extends \Magento\Quote\Model\Quote\Address\Total\AbstractTotal
      */
     public function getBaseFee(
         \TIG\Buckaroo\Model\Method\AbstractMethod $methodInstance,
-        \Magento\Quote\Model\Quote $quote
+        \Magento\Quote\Model\Quote $quote,
+        $inclTax = false
     ) {
         $buckarooPaymentMethodCode = $methodInstance->buckarooPaymentMethodCode;
         if (!$this->configProviderMethodFactory->has($buckarooPaymentMethodCode)) {
@@ -213,6 +214,9 @@ class BuckarooFee extends \Magento\Quote\Model\Quote\Address\Total\AbstractTotal
         $basePaymentFee = trim($configProvider->getPaymentFee());
 
         if (is_numeric($basePaymentFee)) {
+            if($inclTax) {
+                return $basePaymentFee;
+            }
             /**
              * Payment fee is a number
              */
